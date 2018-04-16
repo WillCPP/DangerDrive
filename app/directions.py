e2f0386d35dc1d95d776dfad34cd7c3ff08dd96f
WillCPP <WillCPP@noreply.users.github.com>
2018-04-16 16:23:32 -0400

    Moved directions.py, find_locations.py

---
+++ app/directions.py
@@ -0,0 +1,18 @@
+#
+#  directions.py
+#  Danger Drive
+#  
+#  Created by Madeline Eckhart, William Cupp, and Sean Hearne on 4/12/18.
+#  Copyright © 2018 University of Cincinnati. All rights reserved.
+
+
+import requests
+def directions(x1, y1, x2, y2):
+    key_dir = "AIzaSyAJiJ0E_yddGhKshaTTcbSE8HQmFyoHs4Y"
+    origin_coord = str(x1) + ',' + str(y1)
+    dest_coord = str(x2) + ',' + str(y2)
+    r = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin='+origin_coord+'&destination='+dest_coord+'&key='+ key_dir)
+    print(r.text)
+
+# Cincinnati to Dayton
+directions(31.1031,84.5120,39.7589,84.1916)
---
+++ app/find_locations.py
@@ -0,0 +1,28 @@
+#
+#  find_locations.py
+#  Danger Drive
+#
+#  Created by Madeline Eckhart, William Cupp, and Sean Hearne on 4/12/18.
+#  Copyright © 2018 University of Cincinnati. All rights reserved.
+
+
+import requests
+import json
+def find_address(address):
+    key_java = "AIzaSyDZWXeInvVOUuIe0xLKA65ygwHSRuxayxU"
+    add = address.replace(' ', '+')
+    r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+add+'&key='+ key_java)
+    x = json.loads(r.text)
+    lat = x["results"][0]["geometry"]["location"]["lat"]
+    long = x["results"][0]["geometry"]["location"]["lng"]
+    return (lat, long)
+
+
+def find_ip():
+    r = requests.get('http://ip-api.com/json')
+    x = json.loads(r.text)
+    lat = x["lat"]
+    long = x["lon"]
+    return (lat, long)
+
+print(find_ip())
