431c2557ff6ffa690009f3c38ffd5dbed7b8c3f5
Madeline Eckhart <madelineaeckhart@gmail.com>
2018-04-21 15:08:53 -0400

    radio buttons and reveal successful

---
+++ app/static/scripts/script.js
@@ -0,0 +1,11 @@
+function hide_addr()
+{
+    document.getElementById("addr").className = "hide"
+    document.getElementById("loc").className = ""
+}
+
+function hide_loc()
+{
+    document.getElementById("addr").className = ""
+    document.getElementById("loc").className = "hide"
+}
