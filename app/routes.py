4dd57ab928d5b90b6f270b2b276e2ccd30da23e6
WillCPP <WillCPP@noreply.users.github.com>
2018-04-21 15:10:37 -0400

    Test two way data transfer

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,26 @@
 import requests
 import json
+from flask import Flask, request, render_template
 from app import app
-from Website import *
+import Website
+import directions
+import find_locations
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    
-    return one + two + """Hello""" + three
+    #latlong = find_locations.find_ip()
+
+    #render template for input page
+    #custom render for output page
+    #return Website.one + Website.two + """Lat: """ + str(latlong[0]) + """ Lng: """ + str(latlong[1]) + Website.three
+    return render_template('web_prototype.html')
+
+@app.route('/', methods=['POST'])
+#@app.route('/index', methods=['POST'])
+def data_post():
+    text = request.form['street']
+    processed_text = text.upper()
+    print(processed_text)
+    return processed_text
